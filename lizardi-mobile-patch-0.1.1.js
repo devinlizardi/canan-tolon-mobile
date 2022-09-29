da8e32b6eeba0da28136698039303df29d5243a2
Devin Lizardi <dlizardi@berkeley.edu>
2022-09-29 16:01:26 -0400

    imported codepen

--- lizardi-mobile-patch-0.1.1.js
+++ lizardi-mobile-patch-0.1.1.js
@@ -1,11 +1,33 @@
 /*
 
 */
+let minWidth = 539
+let nav
+let created = false
 
-let minWidth = 1000
+let homePageTable = document.querySelector('table')
 
-const createNav = () => {
-  let container = document.createElement('div')
+const handleResize = () => {
+  console.log(window.innerWidth)
+  if (window.innerWidth < minWidth && !created) {
+    // if too small
+    createNav()
+  } else if (window.innerWidth >= minWidth && nav) {
+    nav.remove()
+    created = false
+  }
 }
 
-console.log("loaded my script");
+const createNav = () => {
+  let tag = document.createElement('p')
+  var main = document.getElementsByClassName('main')[0]
+  
+  tag.id = 'nav-style'
+  
+  main.appendChild(tag)
+  nav = document.getElementById('nav-style')
+  created = true
+}
+
+handleResize()
+window.addEventListener('resize', handleResize)
