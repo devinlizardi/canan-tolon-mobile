62d75f5945fd0076c57e4bcf0a3c515a35c5062a
Devin Lizardi <dlizardi@berkeley.edu>
2022-10-03 17:54:51 -0400

    finished css injection

--- lizardi-mobile-patch-0.1.1.js
+++ lizardi-mobile-patch-0.1.1.js
@@ -1,47 +1,20 @@
 /*
 
 */
-let minWidth = 539
-let created = false
-
-const homePageTable = document.querySelector("table")
-const nav = document.createElement("div")
-
-nav.style.width = "fit-content"
-nav.style.height = "10em"
-nav.style.backgroundColor = "red"
-nav.style.position = "fixed"
-nav.style.top = "0px"
-nav.style.left = "0px"
-
-nav.innerHTML = `<ul>
-    <li><a href=${"#"}>about</a></li>
-    <li><a href=${"#"}>biograghy</a></li>
-    <li><a href=${"#"}>contact</a></li>
-  </ul>
-  `
-
-const handleResize = () => {
-  //console.log(window.outerWidth, window.innerWidth)
-
-  if (window.innerWidth < minWidth && !created) {
-    homePageTable.appendChild(nav)
-    created = true
-  } else if (window.innerWidth >= minWidth) {
-    nav.remove()
-    created = false
-  }
-}
 
 var style = document.createElement("style")
 style.innerHTML = `  
-  @media not (any-hover: hover) {
+  @media (any-hover: none) {
     body {
       position: relative;
       top: calc(50vh - 405px);
     }
-  }`
+  }
+  @media (any-hover: none) and (max-height: 800px) {
+    body {
+      top: calc(50vh - 350px);
+      transform: scale(0.7);
+    }
+  }
+  `
 document.head.append(style)
-
-//handleResize()
-//window.addEventListener("resize", handleResize)
